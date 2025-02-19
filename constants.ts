b0dc04cd8be1093e43dca0b7f903b8b33191c05c
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2025-02-19 15:52:53 -0300

    Refactor to improve readability

---
+++ constants.ts
@@ -0,0 +1,18 @@
+import { Address } from "viem";
+
+export const BALANCER_ROUTER = "0x6A20a4b6DcFF78e6D21BF0dbFfD58C96644DB9cb";
+export const USDC = {
+    address: "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8" as Address,
+    decimals: 6,
+    slot: 0,
+};
+export const USDT = {
+    address: "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0" as Address,
+    decimals: 6,
+    slot: 0,
+};
+export const stataUSDT = {
+    address: "0x978206fae13faf5a8d293fb614326b237684b750" as Address,
+    decimals: 6,
+    slot: 0,
+};
--- index.ts
+++ index.ts
@@ -20,37 +20,17 @@ import { boostedPool_USDC_USDT } from "./mockData";
 import { balancerRouterAbi } from "./balancerRouterAbi";
 import { createAnvil } from "@viem/anvil";
 import { config } from "dotenv";
+import { BALANCER_ROUTER, USDT, stataUSDT, USDC } from "./constants";
 config();
-
-const BALANCER_ROUTER = "0x6A20a4b6DcFF78e6D21BF0dbFfD58C96644DB9cb";
-
-const USDC = {
-    address: "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8" as Address,
-    decimals: 6,
-    slot: 0,
-};
-const USDT = {
-    address: "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0" as Address,
-    decimals: 6,
-    slot: 0,
-};
-const stataUSDT = {
-    address: "0x978206fae13faf5a8d293fb614326b237684b750" as Address,
-    decimals: 6,
-    slot: 0,
-};
 
 const forkUrl = process.env.FORK_URL;
 
 const anvil = createAnvil({
-    // All anvil options are supported & typed.
     forkUrl,
     forkBlockNumber: 7655008n,
 });
 
 await anvil.start();
-
-console.log(`http://${anvil.host}:${anvil.port}`);
 
 const client = createTestClient({
     mode: "anvil",
@@ -60,60 +40,13 @@ const client = createTestClient({
     .extend(publicActions)
     .extend(walletActions);
 
-const testAddress = (await client.getAddresses())[0];
+// setup testAddress via setStorate or impersonateAccount
+const testAddress = await setupTestAddress("setStorage");
 
-// set erc20 balances
-await setTokenBalances(
-    client,
-    testAddress,
-    [USDT.address, USDC.address],
-    [USDT.slot, USDC.slot] as number[],
-    [parseUnits("1000", USDT.decimals), parseUnits("1000", USDC.decimals)]
-);
+// approve tokens to be spent
+await approveTokens();
 
-// set erc4626 token balance
-await approveSpenderOnTokens(
-    client,
-    testAddress,
-    [USDT.address],
-    stataUSDT.address
-);
-await client.writeContract({
-    account: testAddress,
-    chain: sepolia,
-    abi: erc4626Abi,
-    address: stataUSDT.address,
-    functionName: "deposit",
-    args: [parseUnits("1000", USDT.decimals), testAddress],
-});
-
-// approve Permit2 to spend users DAI/USDC, does not include the sub approvals
-await approveSpenderOnTokens(
-    client,
-    testAddress,
-    [USDC.address, stataUSDT.address],
-    PERMIT2
-);
-
-// Here we approve the Vault to spend tokens on the users behalf via Permit2
-for (const token of boostedPool_USDC_USDT.tokens) {
-    await approveSpenderOnPermit2(
-        client,
-        testAddress,
-        token.address as Address,
-        BALANCER_ROUTER
-    );
-
-    if (token.underlyingToken) {
-        await approveSpenderOnPermit2(
-            client,
-            testAddress,
-            token.underlyingToken.address as Address,
-            BALANCER_ROUTER
-        );
-    }
-}
-
+// setup transaction data
 const args = [
     "0x59fa488dda749cdd41772bb068bb23ee955a6d7a",
     [true, false],
@@ -169,6 +102,69 @@ for (let i = 0; i < 100; i++) {
 
 await anvil.stop();
 
-const blockNumber = await client.getBlockNumber();
+// helper functions
 
-console.log(blockNumber);
+async function setupTestAddress(
+    option: "impersonate" | "setStorage"
+): Promise<Address> {
+    let testAddress: Address;
+    if (option === "impersonate") {
+        testAddress = "0x75D06bae37a9c349142fE7cee77804900b1C0EC3";
+        await client.impersonateAccount({
+            address: testAddress,
+        });
+    } else {
+        testAddress = (await client.getAddresses())[0];
+        await setTokenBalances(
+            client,
+            testAddress,
+            [USDT.address],
+            [USDT.slot]
+        );
+    }
+    return testAddress;
+}
+
+async function approveTokens() {
+    await approveSpenderOnTokens(
+        client,
+        testAddress,
+        [USDT.address],
+        stataUSDT.address
+    );
+    await client.writeContract({
+        account: testAddress,
+        chain: sepolia,
+        abi: erc4626Abi,
+        address: stataUSDT.address,
+        functionName: "deposit",
+        args: [parseUnits("1000", USDT.decimals), testAddress],
+    });
+
+    // approve Permit2 to spend users DAI/USDC, does not include the sub approvals
+    await approveSpenderOnTokens(
+        client,
+        testAddress,
+        [USDC.address, stataUSDT.address],
+        PERMIT2
+    );
+
+    // Here we approve the Vault to spend tokens on the users behalf via Permit2
+    for (const token of boostedPool_USDC_USDT.tokens) {
+        await approveSpenderOnPermit2(
+            client,
+            testAddress,
+            token.address as Address,
+            BALANCER_ROUTER
+        );
+
+        if (token.underlyingToken) {
+            await approveSpenderOnPermit2(
+                client,
+                testAddress,
+                token.underlyingToken.address as Address,
+                BALANCER_ROUTER
+            );
+        }
+    }
+}
